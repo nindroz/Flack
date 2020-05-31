3bc0b394e00333e343c31b1c790f60df693e7016
Nindroz <mantha.hitesh@gmail.com>
2020-05-30 22:25:34 -0700

    worked on channel maker

--- application.py
+++ application.py
@@ -1,12 +1,14 @@
 import os
 
-from flask import Flask,render_template,request,session,redirect, url_for
+from flask import Flask,render_template,request,session,redirect, url_for, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "fdjdkfksajfasdj"
 socketio = SocketIO(app)
 
+channels=[]
+messages={}
 
 @app.route("/",methods=['GET',"POST"])
 def index():
@@ -16,20 +18,36 @@ def index():
     #makes sure that session is null if it doesent exist
     if(not("displayName" in session)):
         session["displayName"]=None
+    
     #sets session and logs in
     if(session['displayName']==None):
         session['displayName']=name
-        return render_template("index.html",name=session['displayName'])
+        if(session["displayName"]==None):
+            #loads page for first time when user hasnt submitted a name
+            return render_template("index.html")
+    return redirect(url_for('dashboard'))
     
-    else:
-        return render_template("index.html",name=session['displayName'])
-    
-    #loads page for first time when user hasnt submitted a name
-    if(name is None):
-        return render_template("index.html")
+   
+
+@app.route("/dashboard",methods=['post','get'])
+def dashboard():
+    return render_template("dashboard.html",name=session['displayName'],channels=channels)
 
 #test purposes
 @app.route("/pop")
 def pop():
     session.pop("displayName")
     return redirect('/')
+
+@app.route("/channelMake", methods=['POST'])
+def channelMake():
+    name = request.form.get("channel")
+    channels.append(name)
+    return redirect(url_for('dashboard'))
+
+
+@socketio.on("make channel")
+def channelM(data):
+    name=data['name']
+    channels.append(name)
+    emit("display name", {"channelName":name}, broadcast=True)
